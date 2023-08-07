9b65bc9e6fbf16766f260078e3c543f8789cd041
PyFra22 <116340901+PyFra22@users.noreply.github.com>
2023-08-07 10:41:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+let scorePoint = 20;
+
+document.querySelector('.check').addEventListener('click',
+function(){
+    const guessNumber = Number (document.querySelector('.guess').value);
+    if(!guessNumber){
+        document.querySelector('.message').textContent = 'No number Give⛔️';
+    }else if(guessNumber>secretNumber){
+        document.querySelector('.message').textContent = 'Too High ⬆️';
+        scorePoint = scorePoint - 1;
+        document.querySelector('.score').textContent = scorePoint;
+    }else if(guessNumber<secretNumber){
+        document.querySelector('.message').textContent = 'Too Low ⬇️';
+        scorePoint = scorePoint - 1;
+        document.querySelector('.score').textContent = scorePoint;
+    }else if(guessNumber === secretNumber){
+        document.querySelector('.message').textContent = 'Correct Number 🏆';
+        document.querySelector('.highscore').textContent = scorePoint;
+        //document.querySelector('.scorePoint').textContent = secretNumber;
+    }
+});
+
+document.querySelector('.again').addEventListener('click',
+function(){
+    scorePoint = 20;
+    document.querySelector('.score').textContent = scorePoint;
+    console.log('Ciao');
+});
